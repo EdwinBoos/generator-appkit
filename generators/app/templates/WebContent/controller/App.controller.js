a4905ae567807a2be78e60a8bb8999386f9c96d1
INFLUENZ <edwin.boos@gmx.de>
2018-12-10 14:17:51 +0100

    Update App.controller.js

--- generators/app/templates/WebContent/controller/App.controller.js
+++ generators/app/templates/WebContent/controller/App.controller.js
@@ -6,10 +6,8 @@ sap.ui.define(
 
     /**
      *
-     * NOTE! the extend() method does not inherit from a super class, like in most oop-languages.
-     * What it does instead is that the 'super' controller will us only lend all functions which are declared
-     * in the super controller. Hence there is no super keyword to access the declared functions in the super controller.
-     * Since the methods got added to the child controller's instance, we need to access them with the 'this' keyword.
+     * NOTE! We are extending the SuperController, and it lends us all the functions declared.
+     * You can easily acccess the functions with the 'this' keyword.
      *
      */
     return SuperController.extend("<%= moduleName %>.controller.App", {});
