154bce5cf8b007d2b191808be053361f32ee1a8a
INFLUENZ <edwin.boos@gmx.de>
2018-12-10 14:18:48 +0100

    Update App.controller.js

--- generators/app/templates/WebContent/controller/App.controller.js
+++ generators/app/templates/WebContent/controller/App.controller.js
@@ -4,12 +4,6 @@ sap.ui.define(
   SuperController => {
     "use strict";
 
-    /**
-     *
-     * NOTE! We are extending the SuperController, and it lends us all the functions declared.
-     * You can easily acccess the functions with the 'this' keyword.
-     *
-     */
     return SuperController.extend("<%= moduleName %>.controller.App", {});
   }
 );
