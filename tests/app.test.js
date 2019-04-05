357bf4aeebbf039610744576660ffaf1dbbbee9c
Edwin Boos <boosedwin1@gmail.com>
2019-04-05 15:23:15 +0200

    First test

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,20 +2,30 @@
 const path = require("path");
 const assert = require("yeoman-assert");
 const helpers = require("yeoman-test");
+const rimraf = require("rimraf");
 
 describe("generator-appkit", () => {
   beforeEach(() => {
     return helpers
       .run(path.join(__dirname, "../generators/app"))
+      .inDir(path.join(__dirname, "tmp"))
       .withPrompts({
-        name: "test"
-      })
-      .then(() => {
-        it("creates file", () => {
-          assert.file([""]);
-        });
+        name: "test",
+	moduleName : "de.test",
+	controllerName : "Main.controller.js",
+	serviceName : "SERVICE_NAME",
+	outputPath : "."
 
-        
       });
+   });
+  
+    afterEach(() => {
+    	    rimraf.sync(path.join(__dirname, 'tmp'));
+	  });
+   it("creates file", () => {
+     assert.file(path.join(__dirname, "tmp/.babelrc"));
+   });
+
+
   });
-});
+
